c7906edeb2a7b7135f2d4d4d2e551682fd937fd0
wuuconix <1521900139@qq.com>
2022-05-13 18:48:38 +0800

    修改config/vue.js

--- config/vue_demo.js
+++ config/vue_demo.js
@@ -1,5 +1,5 @@
 // 按实际情况填写后将文件名修改为vue.js
 module.exports = {
-    baseURI: "http://127.0.0.1/",
-    apiURI: "http://127.0.0.1/api"
+    baseURI: "http://localhost/",
+    apiURI: "http://localhost/api"
 }
